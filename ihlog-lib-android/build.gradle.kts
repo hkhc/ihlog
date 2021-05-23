4f60ee88386f8c60a0b7d04c5c5ce2b50b07c3ca
Herman Cheung <herman.kh.cheung@gmail.com>
2021-05-23 14:17:16 +0800

    Not to mimify android library

--- ihlog-lib-android/build.gradle.kts
+++ ihlog-lib-android/build.gradle.kts
@@ -67,7 +67,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
         getByName("debug") {
